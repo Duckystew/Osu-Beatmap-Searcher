31494dbb8cc0d384b88c4462ba457791e0cfc140
Ankpudding <jikai.xiang2012@gmail.com>
2025-05-02 20:35:05 +0200

    Fixed bug
    Fixed a bug where the program would try to add the same beatmap several times.

--- filewatcher.py
+++ filewatcher.py
@@ -20,18 +20,21 @@ def open_file(file_path):
 def searchBeatmaps(directory, extension, num_files=10):
 
     while True:
-
+        empty = True
         for filename in os.listdir(directory):
 
             if filename.endswith(extension):
+                empty = False
 
                 file_path = os.path.join(directory, filename)
 
                 print(f"Opening beatmap in osu!: {file_path}")
                 open_file(file_path)
-                time.sleep(0.1)
 
-        time.sleep(0.5)
+        if empty == False:
+            time.sleep(5)
+        else:
+            time.sleep(0.5)
 
 if __name__ == "__main__":
     print("--------------------------------------")
